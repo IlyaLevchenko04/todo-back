edde6f1960d8d3d54d44d56642a5d03227c763e6
IlyaLevchenko04 <nneo2086@gmail.com>
2023-09-02 17:24:12 +0300

    added Swagger doc
    Added swagger doc. Plus added middleware 'isValidObjId', which compare id with standarts of objId.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const http = require('http');
 const router = require('./routes/api/v1/index.js');
 const errorHandler = require('./middlewares/errorHandler.js');
@@ -11,6 +13,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
+
+const swaggerOptions = {
+  customCss: '.swagger-ui .topbar { display: none }',
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Todo API Specification',
+      description: 'This routes allow to manipulate with todos and users.',
+      contact: {
+        name: 'API Support',
+        email: 'nneo2086@gmail.com/',
+      },
+      version: '1.0',
+    },
+    servers: [
+      {
+        description: 'Public server (v1)',
+        url: `https://products-api-umhe.onrender.com/api/v1/`,
+      },
+      {
+        description: 'Test server (v1)',
+        url: `http://localhost:${process.env.PORT}/api/v1/`,
+      },
+    ],
+    schemes: ['https'],
+  },
+  apis: ['server.js', './routes/**/*.js'],
+};
+const apiSpecification = swaggerJsdoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(apiSpecification));
 
 app.use('/api/v1', router);
 
--- server.js
+++ server.js
@@ -4,12 +4,3 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_HOST).then(() => {
   console.log('Connected to MongoDB');
 });
-// .then(() => {
-//   server.listen(process.env.PORT, () => {
-//     console.log('Database connection successful');
-//   });
-// })
-// .catch(err => {
-//   console.log(err.message);
-//   process.exit(1);
-// });
