3b8c2aa48a60bc493a98b7cb260f3edc75ad917a
IlyaLevchenko04 <nneo2086@gmail.com>
2023-09-22 13:11:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const { server } = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`${process.env.DB_HOST}`).then(() => {
-  console.log('Connected to MongoDB');
-});
+mongoose
+  .connect(
+    `mongodb+srv://IlyaLevchenko:45HOIv5ejHF4mWMY@contactsdb.hlihyqp.mongodb.net/todo?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log('Connected to MongoDB');
+  });
