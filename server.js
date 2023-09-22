34b380640fce37117e2ad279d836f177d51eb7f8
IlyaLevchenko04 <nneo2086@gmail.com>
2023-09-22 13:01:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { server } = require('./app');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_HOST).then(() => {
+mongoose.connect(`${process.env.DB_HOST}`).then(() => {
   console.log('Connected to MongoDB');
 });
